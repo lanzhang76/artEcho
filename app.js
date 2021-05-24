ee411c2198960ef59151a8730c2c30b8a49073bc
Lan Zhang <zhany016@newschool.edu>
2021-05-23 23:05:16 -0400

    raycast hover and click detection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Raycast from "./js/raycast";
 
 export default class Sketch {
   constructor(options) {
@@ -19,8 +20,15 @@ export default class Sketch {
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
+    this.currentColor = 0x00ff00;
+    this.currentColor2 = 0x0000ff;
+
+    this.raycast = new Raycast(this.camera, this.scene);
+    this.mouse = new THREE.Vector2();
+
     this.resize();
     this.setupResize();
+    this.setupMouse();
     this.addObject();
     this.render();
   }
@@ -29,25 +37,75 @@ export default class Sketch {
     window.addEventListener("resize", this.resize.bind(this));
   }
 
+  setupMouse() {
+    window.addEventListener("mousemove", this.onMouseMove.bind(this), false);
+    window.addEventListener("click", () => {
+      this.raycast.clickInfo(this.mouse);
+    });
+  }
+
+  onMouseMove(event) {
+    this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  }
+
   resize() {
     this.width = this.container.offsetWidth;
     this.height = this.container.offsetHeight;
     this.renderer.setSize(this.width, this.height);
     this.camera.aspect = this.width / this.height;
+    this.camera.position.y = 0.1;
     this.camera.updateProjectionMatrix();
   }
 
   addObject() {
     this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    this.material = new THREE.MeshNormalMaterial();
+    this.material = new THREE.MeshBasicMaterial({
+      color: this.currentColor,
+      // wireframe: true,
+    });
+    this.material2 = new THREE.MeshBasicMaterial({
+      color: this.currentColor2,
+      // wireframe: true,
+    });
 
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(this.mesh);
+    this.object1 = new THREE.Mesh(this.geometry, this.material);
+    this.object2 = new THREE.Mesh(this.geometry, this.material2);
+    this.object1.position.set(-0.2, 0, 0);
+    this.object2.position.set(0.2, 0, 0);
+    this.object1.name = "green box";
+    this.object2.name = "blue box";
+
+    this.scene.add(this.object1);
+    this.scene.add(this.object2);
+    this.addFloor();
+  }
+
+  addFloor() {
+    this.floor = new THREE.PlaneBufferGeometry(10, 2, 1, 1);
+    this.material_floor = new THREE.MeshBasicMaterial({ color: "#ffffff", side: THREE.DoubleSide });
+    this.floorObject = new THREE.Mesh(this.floor, this.material_floor);
+    this.floorObject.position.set(0, -0.11, 0);
+    this.floorObject.rotation.set(Math.PI / 2, 0, 0);
+    this.floorObject.name = "floor";
+    this.scene.add(this.floorObject);
+  }
+
+  addLight() {
+    this.color = 0xffffff;
+    this.intensity = 1;
+    this.light = new THREE.PointLight(this.color, this.intensity);
+    this.light.position.set(0, 30, 30);
+    this.scene.add(this.light);
   }
 
   render() {
-    this.mesh.rotation.x = this.time / 2000;
-    this.mesh.rotation.y = this.time / 1000;
+    // this.object1.rotation.x = this.time / 20;
+    // this.object1.rotation.y = this.time / 10;
+    // this.object2.rotation.x = this.time / 20;
+    // this.object2.rotation.y = this.time / 10;
+
+    this.raycast.update(this.mouse);
 
     this.renderer.render(this.scene, this.camera);
     this.time += 0.05;
---
+++ js/raycast.js
@@ -0,0 +1,59 @@
+import * as THREE from "three";
+import gsap from "gsap";
+
+export default class Raycast {
+  constructor(camera, scene) {
+    this.scene = scene;
+    this.camera = camera;
+    this.raycaster = new THREE.Raycaster();
+    this.goalColor = 0xffff00;
+    this.INTERSECTED = null;
+  }
+
+  getHexColor(hexStr) {
+    var c = new THREE.Color();
+    c.set(hexStr);
+    return `#` + c.getHexString(); // "c08000"
+  }
+
+  clickInfo(mouse) {
+    this.checkpoints = mouse;
+    this.raycaster.setFromCamera(this.checkpoints, this.camera);
+    this.click_intersects = this.raycaster.intersectObjects(this.scene.children);
+
+    var textBox = document.querySelector("#selected");
+    if (this.intersects.length) {
+      this.objectName = this.click_intersects[0].object.name;
+      textBox.innerText = `${this.click_intersects[0].object.name} is selected`;
+
+      if (this.objectName.toString() != "floor") {
+        this.cameraAnimation();
+      }
+    } else {
+      textBox.innerText = `nothing is selected`;
+    }
+  }
+
+  cameraAnimation() {
+    this.lookPos = this.intersects[0].object.position;
+    this.coords = { x: this.camera.position.x, y: this.camera.position.y };
+    gsap.to(this.coords, { x: this.lookPos.x, y: this.lookPos.y, onUpdate: () => this.camera.position.set(this.coords.x, this.coords.y, this.camera.position.z) });
+  }
+
+  update(mouse) {
+    this.mouse = mouse;
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    this.intersects = this.raycaster.intersectObjects(this.scene.children);
+
+    if (this.INTERSECTED) {
+      this.INTERSECTED.material.color.set(this.currentColor);
+      this.INTERSECTED = undefined;
+    }
+
+    if (this.intersects.length) {
+      this.INTERSECTED = this.intersects[0].object;
+      this.currentColor = this.INTERSECTED.material.color.getHex();
+      this.INTERSECTED.material.color.set(this.goalColor);
+    }
+  }
+}
