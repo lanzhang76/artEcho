d40a1b8ac2704938852a5999bc8e0743feac3f72
Lan Zhang <zhany016@newschool.edu>
2021-06-22 14:21:55 -0400

    rooms

--- three_gltf/src/data/models.js
+++ three_gltf/src/data/models.js
@@ -2,32 +2,48 @@ export const models = [
   {
     name: "chamber1",
     data: [
-      { name: "Geochelone carbonaria", path: "../models/c1/geochelone/USNM_222486-100k-2048.gltf", mesh: null, box: null, position: { x: 3, y: 0.4, z: -3 }, rotation: { x: 0, y: 0, z: 0 }, offset: { x: 0, y: 0, z: 0 }, theta: Math.PI / 2, stare_dist: 1.5 },
-      { name: "Eremotherium laurillardi", path: "../models/c1/eremotherium/eremotherium_laurillardi-150k-4096.gltf", mesh: null, box: null, position: { x: 3, y: 2, z: 3 }, rotation: { x: 0, y: 0, z: 0 }, offset: { x: 0, y: 0, z: 0 }, theta: (Math.PI * 3) / 4, stare_dist: 3 },
-      { name: "Mammuthus primigenius (Blumbach)", path: "../models/c1/mammoth/woolly-mammoth-100k-4096.gltf", mesh: null, box: null, position: { x: -3, y: 2, z: 3 }, rotation: { x: 0, y: (Math.PI * 5) / 4, z: 0 }, offset: { x: 0, y: 0, z: 0 }, theta: -Math.PI / 4, stare_dist: 3 },
+      { name: "Geochelone carbonaria", path: "../models/c1/geochelone/USNM_222486-100k-2048.gltf", mesh: null, box: null, scale: 1, position: { x: 3, y: 1, z: -3 }, rotation: { x: 0, y: 0, z: 0 }, offset: { x: 0, y: 1, z: 0 }, theta: Math.PI / 2, stare_dist: 1.5 },
+      {
+        name: "Eremotherium laurillardi",
+        path: "../models/c1/eremotherium/eremotherium_laurillardi-150k-4096.gltf",
+        mesh: null,
+        box: null,
+        scale: 0.001,
+        position: { x: 3, y: 1, z: 3 },
+        rotation: { x: 0, y: (Math.PI * 5) / 4, z: 0 },
+        offset: { x: 0, y: 0, z: 0 },
+        theta: Math.PI * 3,
+        stare_dist: 3,
+      },
+      { name: "Mammuthus primigenius (Blumbach)", path: "../models/c1/mammoth/woolly-mammoth-100k-4096.gltf", mesh: null, box: null, scale: 1, position: { x: -3, y: 2, z: 3 }, rotation: { x: 0, y: (Math.PI * 5) / 4, z: 0 }, offset: { x: 0, y: 0, z: 0 }, theta: -Math.PI / 4, stare_dist: 3 },
       {
         name: "Triceratops horridus",
         path: "../models/c1/tricera/Triceratops_horridus_Marsh_1889-150k-4096.gltf",
         mesh: null,
         box: null,
-        position: { x: -3, y: 2, z: -3 },
+        scale: 1,
+        position: { x: -3, y: 2, z: -3.5 },
         rotation: { x: -Math.PI / 8, y: -Math.PI / 4, z: Math.PI / 4 },
         offset: { x: 0, y: 0.7, z: 0 },
         theta: Math.PI / 4,
-        stare_dist: 3.5,
+        stare_dist: 3,
       },
-
     ],
     soundsFiles: [
-        { name: "Geochelone carbonaria", path: "../Background-Sounds/Chamber1/Object1.mp3", position: { x: 5, y: 0, z: -5 } },
+      { name: "Geochelone carbonaria", path: "../Background-Sounds/Chamber1/Object1.mp3", position: { x: 5, y: 0, z: -5 } },
       { name: "Eremotherium laurillardi", path: "../Background-Sounds/Chamber1/Object2.mp3", position: { x: 5, y: 0, z: 5 } },
-      { name: "Mammuthus primigenius (Blumbach)", path: "../Background-Sounds/Chamber1/Object3.mp3", position: { x: -5, y: 0, z: 5 }},
-      { name: "Triceratops horridus", path: "../Background-Sounds/Chamber1/Object4.mp3", position: { x: -5, y: 0, z: -5 },}
+      { name: "Mammuthus primigenius (Blumbach)", path: "../Background-Sounds/Chamber1/Object3.mp3", position: { x: -5, y: 0, z: 5 } },
+      { name: "Triceratops horridus", path: "../Background-Sounds/Chamber1/Object4.mp3", position: { x: -5, y: 0, z: -5 } },
     ],
   },
   {
     name: "chamber2",
-    data: [{ name: "vase", path: "../models/vase/vase-f1991-150k-4096.gltf", mesh: null, box: null, position: { x: -2, y: 0.1, z: -2 }, theta: Math.PI * 2, stare_dist: 0.5 }],
+    data: [
+      { name: "Baluster vase", path: "../models/vase1/f1980_193-150k-4096.gltf", mesh: null, box: null, scale: 1, position: { x: -2, y: 0.1, z: -22 }, rotation: { x: 0, y: 0, z: 0 }, offset: { x: 0, y: 0, z: 0 }, theta: Math.PI * 2, stare_dist: 0.5 },
+      { name: "Beaker-shaped vase", path: "../models/vase2/baluster_vase-150k-4096.gltf", mesh: null, box: null, scale: 1, position: { x: 2, y: 0.1, z: -22 }, rotation: { x: 0, y: 0, z: 0 }, offset: { x: 0, y: 0, z: 0 }, theta: Math.PI * 2, stare_dist: 0.5 },
+      { name: "fangyi", path: "../models/fangyi/fangyi-150k-4096.gltf", mesh: null, box: null, scale: 1, position: { x: -2, y: 0.1, z: -20 }, rotation: { x: 0, y: 0, z: 0 }, offset: { x: 0, y: 0, z: 0 }, theta: Math.PI * 2, stare_dist: 0.5 },
+      { name: "ewer", path: "../models/ewer/lidded_ewer-150k-4096.gltf.gltf", mesh: null, box: null, scale: 1, position: { x: 2, y: 0.1, z: -20 }, rotation: { x: 0, y: 0, z: 0 }, offset: { x: 0, y: 0, z: 0 }, theta: Math.PI * 2, stare_dist: 0.5 },
+    ],
     soundsFiles: [],
   },
   {
@@ -45,8 +61,11 @@ export const models = [
   },
   {
     name: "rooms",
-    data: [{ name: "chamber1-room", path: "../models/rooms/chamber1/test.gltf", mesh: null, box: null, position: { x: 0, y: 0, z: 0 } }],
+    data: [
+      { name: "chamber1-room", path: "../models/rooms/chamber1/chamber1.gltf", mesh: null, box: null, scale: 2.5, position: { x: 0, y: 0, z: 0 } },
+      { name: "chamber2-room", path: "../models/rooms/chamber2/chamber2.gltf", mesh: null, box: null, scale: 2.5, position: { x: 0, y: 0, z: -20 } },
+      // { name: "chamber3-room", path: "../models/rooms/chamber3/chamber3.gltf", mesh: null, box: null, scale: 2.5, position: { x: 0, y: 0, z: -40 } },
+      // { name: "chamber4-room", path: "../models/rooms/chamber4/chamber4.gltf", mesh: null, box: null, scale: 2.5, position: { x: 0, y: 0, z: -90 } },
+    ],
   },
 ];
-
-
--- three_gltf/src/js/sketch.js
+++ three_gltf/src/js/sketch.js
@@ -5,7 +5,6 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { GUI } from "three/examples/jsm/libs/dat.gui.module.js";
 import { models } from "../data/models";
 
-
 const audioLoader = new THREE.AudioLoader();
 export class Sketch {
   constructor(options) {
@@ -15,7 +14,7 @@ export class Sketch {
 
     this.scene = new THREE.Scene();
 
-    this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.01, 100);
+    this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.01, 500);
     this.camera.position.set(0, 1, 0);
     this.camera.rotation.order = "YXZ";
 
@@ -39,12 +38,14 @@ export class Sketch {
     this.inChamber = true;
     this.currentModels = [];
     this.activated = false;
+    this.loaded = false;
+    this.keyPressed = false;
 
     this.ogPos = [
       { name: "chamber1", x: 0, y: 1, z: 0 },
       { name: "chamber2", x: 0, y: 1, z: -20 },
-      { name: "chamber3", x: 0, y: 0, z: 0 },
-      { name: "chamber4", x: 0, y: 0, z: 0 },
+      { name: "chamber3", x: 0, y: 1, z: -40 },
+      { name: "chamber4", x: 0, y: 1, z: -90 },
     ];
 
     this.rooms = models[4].data;
@@ -112,6 +113,7 @@ export class Sketch {
           console.log("room" + (xhr.loaded / xhr.total) * 100 + "% loaded");
         }
       );
+      this.loaded = true;
       this.render();
     }
 
@@ -119,25 +121,47 @@ export class Sketch {
     if (this.chamber == 1 && this.inChamber == true) {
       this.currentModels = this.chamber1;
       for (let model of this.chamber1) {
-        this.loader.load(
-          model.path,
-          (gltf) => {
-            model.mesh = gltf.scene;
-            this.scene.add(model.mesh);
-
-            model.box = new THREE.Box3().setFromObject(model.mesh);
-            model.mesh.position.set(model.position.x + model.offset.x, model.box.getSize(new THREE.Vector3()).y / 2 + model.offset.y, model.position.z + model.offset.z);
-            model.mesh.rotation.set(model.rotation.x, model.rotation.y, model.rotation.z);
-          },
-          (xhr) => {
-            // while loading:
-            console.log("models are " + (xhr.loaded / xhr.total) * 100 + "% loaded");
-          }
-        );
+        this.loadThisModel(model);
       }
     }
 
+    // for (let model of this.chamber1) {
+    //   this.loadThisModel(model);
+    // }
+
+    // for (let model of this.chamber2) {
+    //   this.loadThisModel(model);
+    // }
+
     console.log(this.currentModels);
+  }
+
+  load2() {
+    if (this.chamber == 2 && this.inChamber == true) {
+      this.currentModels = this.chamber2;
+      for (let model of this.chamber2) {
+        this.loadThisModel(model);
+      }
+    }
+  }
+
+  loadThisModel(model) {
+    this.loader.load(
+      model.path,
+      (gltf) => {
+        model.mesh = gltf.scene;
+        this.scene.add(model.mesh);
+
+        model.box = new THREE.Box3().setFromObject(model.mesh);
+        model.mesh.scale.set(model.scale, model.scale, model.scale);
+        model.mesh.position.set(model.position.x + model.offset.x, (model.box.getSize(new THREE.Vector3()).y * model.scale) / 2 + model.offset.y, model.position.z + model.offset.z);
+        model.mesh.rotation.set(model.rotation.x, model.rotation.y, model.rotation.z);
+      },
+      (xhr) => {
+        // while loading:
+        console.log("models are " + (xhr.loaded / xhr.total) * 100 + "% loaded");
+      }
+    );
   }
 
   setupGUI() {
@@ -167,11 +191,11 @@ export class Sketch {
   }
 
   onKeyDown(event) {
-    if (this.activated && this.controlPanel.VIEWmode == false) {
+    if (this.activated && this.loaded == true) {
       //
       // ** Chamber Navigation **
       //
-      // 1. camera rotation: left and right
+      // 1. camera rotation: left and right at ORIGIN
       if (event.keyCode == 37 && (event.ctrlKey || event.shiftKey)) {
         console.log("rotate left");
         this.tiltCam = true;
@@ -187,15 +211,14 @@ export class Sketch {
         console.log("tilt up");
         this.tiltCam = true;
         this.pointRef.phi += Math.PI / 6;
-      } else if (event.keyCode == 40 && (event.ctrlKey || event.shiftKey) && this.pointRef.phi > (-Math.PI * 3) / 4) {
+      } else if (event.keyCode == 40 && (event.ctrlKey || event.shiftKey) && this.pointRef.phi > (-Math.PI * 3) / 8) {
         console.log("tilt down");
         this.tiltCam = true;
         this.pointRef.phi -= Math.PI / 6;
       }
 
-      // 3. select assets 1-currentModels.length
+      // 3. select assets
       if (event.keyCode >= 48 && event.keyCode <= 54 && event.ctrlKey == false && event.shiftKey == false) {
-        console.log(event.shiftKey);
         switch (event.keyCode) {
           case 48: // 0 is orginal point
             // reset camera angle and position
@@ -252,55 +275,63 @@ export class Sketch {
     }
 
     //
-    // ** object navigation**
+    // ** OBJECT navigation**
     //
 
-    if (this.activated && this.controlPanel.VIEWmode == true) {
+    if (this.activated && this.controlPanel.VIEWmode == true && this.loaded == true) {
       if (event.shiftKey == false) {
         //Move Around Object Navigation
         switch (event.keyCode) {
+          case 48: // 0
+            // reset camera angle and position
+            this.moveBackToCenter();
+            this.textBox.innerText = `moved back to center`;
+            break;
+
           case 8: // del button
             // reset camera angle and position
             this.moveBackToCenter();
             this.textBox.innerText = `moved back to center`;
             break;
 
-          case 27: // escape
-            this.moveBackToCenter();
-            this.textBox.innerText = `moved back to center`;
-            break;
-
           case 37 /*Left*/:
+            // rotate AROUND object
             this.rotateLeftObject();
             break;
 
           case 39 /*Right*/:
+            // rotate AROUND object
             this.rotateRightObject();
             break;
         }
       }
 
-      if (event.keyCode == 38 && (event.ctrlKey || event.shiftKey) && this.point.y < this.controlPanel.INTERSECTED.position.y + Math.PI / 3) {
+      //
+      // ** OBJECT navigation** && camera TILT
+      //
+
+      if (event.keyCode == 38 && (event.ctrlKey || event.shiftKey) && this.point.y < this.controlPanel.INTERSECTED.position.y + Math.PI / 6) {
         // up
         this.point.y += Math.PI / 6;
-      } else if (event.keyCode == 40 && (event.ctrlKey || event.shiftKey) && this.point.y > this.controlPanel.INTERSECTED.position.y - Math.PI / 4) {
+      } else if (event.keyCode == 40 && (event.ctrlKey || event.shiftKey) && this.point.y > this.controlPanel.INTERSECTED.position.y - Math.PI / 6) {
         // down
         this.point.y -= Math.PI / 6;
       }
 
       if (event.keyCode == 37 && (event.ctrlKey || event.shiftKey)) {
         // right
-        if (this.point.z < 0 && this.point.x > this.controlPanel.INTERSECTED.position.x - Math.PI / 3) {
+        if (this.point.z < 0 && this.point.x > this.controlPanel.INTERSECTED.position.x - Math.PI / 6) {
           this.point.x -= Math.PI / 4;
-        } else if (this.point.z > 0 && this.point.x < this.controlPanel.INTERSECTED.position.x + Math.PI / 3) {
+          console.log(this.position.x);
+        } else if (this.point.z > 0 && this.point.x < this.controlPanel.INTERSECTED.position.x + Math.PI / 6) {
           // object placed on +z axis
           this.point.x += Math.PI / 4;
         }
       } else if (event.keyCode == 39 && (event.ctrlKey || event.shiftKey)) {
         // left
-        if (this.point.z < 0 && this.point.x < this.controlPanel.INTERSECTED.position.x + Math.PI / 3) {
+        if (this.point.z < 0 && this.point.x < this.controlPanel.INTERSECTED.position.x + Math.PI / 6) {
           this.point.x += Math.PI / 4;
-        } else if (this.point.z > 0 && this.point.x > this.controlPanel.INTERSECTED.position.x - Math.PI / 3) {
+        } else if (this.point.z > 0 && this.point.x > this.controlPanel.INTERSECTED.position.x - Math.PI / 4) {
           // object placed on +z axis
           this.point.x -= Math.PI / 4;
         }
@@ -310,7 +341,6 @@ export class Sketch {
 
   rotateRightObject() {
     this.controlPanel.INTERSECTED.theta -= Math.PI / 4;
-    console.log(this.currentModels[this.controlPanel.currentSelected].theta);
   }
 
   rotateLeftObject() {
@@ -325,22 +355,22 @@ export class Sketch {
   }
 
   addFloor() {
-    this.floor = new THREE.PlaneBufferGeometry(100, 100, 1, 1);
+    this.floor = new THREE.PlaneBufferGeometry(100, 150, 1, 1);
     this.material_floor = new THREE.MeshBasicMaterial({ color: "#131528", side: THREE.DoubleSide });
     this.floorObject = new THREE.Mesh(this.floor, this.material_floor);
-    this.floorObject.position.set(0, -0.01, 0);
+    this.floorObject.position.set(0, -0.01, -50);
     this.floorObject.rotation.set(Math.PI / 2, 0, 0);
     this.floorObject.name = "floor";
     this.scene.add(this.floorObject);
 
-    this.chamberFloor = new THREE.PlaneBufferGeometry(10, 10, 1, 1);
-    this.chamber_mat = new THREE.MeshBasicMaterial({ color: "#f8f8ff", side: THREE.DoubleSide });
-    this.chamber1 = new THREE.Mesh(this.chamberFloor, this.chamber_mat);
-    this.chamber1.position.set(0, 0.01, -20);
-    this.chamber1.rotation.set(Math.PI / 2, 0, 0);
-    this.scene.add(this.chamber1);
+    // this.chamberFloor = new THREE.PlaneBufferGeometry(10, 10, 1, 1);
+    // this.chamber_mat = new THREE.MeshBasicMaterial({ color: "#f8f8ff", side: THREE.DoubleSide });
+    // this.chamber1 = new THREE.Mesh(this.chamberFloor, this.chamber_mat);
+    // this.chamber1.position.set(0, 0.01, -20);
+    // this.chamber1.rotation.set(Math.PI / 2, 0, 0);
+    // this.scene.add(this.chamber1);
 
-    this.grid = new THREE.GridHelper(100, 20);
+    // this.grid = new THREE.GridHelper(100, 300);
     this.scene.add(this.grid);
   }
 
@@ -348,17 +378,13 @@ export class Sketch {
     this.color = 0xffffff;
     this.intensity = 1;
     this.light = new THREE.PointLight(this.color, this.intensity);
-    this.light_vase = new THREE.DirectionalLight(this.color, this.intensity, 10);
+    this.light_1_2 = new THREE.PointLight(this.color, this.intensity, 20);
     this.ambient = new THREE.AmbientLight(0x404040, this.intensity);
-    this.light.position.set(0, 50, 0.866);
-
-    const targetObject = new THREE.Object3D();
-    targetObject.position.set(-0.5, 0, 0);
-    this.scene.add(targetObject);
-    this.light_vase.target = targetObject;
+    this.light.position.set(2, 3, 0.866);
+    this.light_1_2.position.set(2, 3, 3);
 
     this.scene.add(this.light);
-    this.scene.add(this.light_vase);
+    this.scene.add(this.light_1_2);
     this.scene.add(this.ambient);
   }
 
@@ -372,14 +398,13 @@ export class Sketch {
       });
       info.sound = sound;
       const geometry = new THREE.BoxGeometry();
-      const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-      const sphere = new THREE.Mesh( geometry, material );
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const sphere = new THREE.Mesh(geometry, material);
       sphere.position.copy(info.position);
       sphere.visible = false;
       sphere.add(sound);
       this.scene.add(sphere);
-    })
-
+    });
   }
 
   setActivated() {
@@ -406,9 +431,13 @@ export class Sketch {
       gsap.fromTo(
         this.camera.position,
         {
-          x: this.ogPos[0].x,
-          y: this.ogPos[0].y,
-          z: this.ogPos[0].z,
+          duration: 3,
+          x: this.camera.position.x,
+          y: this.camera.position.y,
+          z: this.camera.position.z,
+          // x: this.ogPos[0].x,
+          // y: this.ogPos[0].y,
+          // z: this.ogPos[0].z,
         },
         {
           duration: 3,
@@ -429,18 +458,22 @@ export class Sketch {
     this.controlPanel.initialMove = true;
     this.tiltCam = true;
     this.clearTarget();
-    gsap.to(this.point, {
-      duration: 2,
-      x: this.pointRef.radius * Math.sin(this.pointRef.phi) * Math.cos(this.pointRef.theta),
-      y: this.pointRef.radius * Math.cos(this.pointRef.phi),
-      z: this.pointRef.radius * Math.sin(this.pointRef.phi) * Math.sin(this.pointRef.theta),
-    });
+    gsap.fromTo(
+      this.point,
+      { duration: 5, x: this.point.x, y: this.point.y, z: this.point.z },
+      {
+        duration: 5,
+        x: this.pointRef.radius * Math.sin(this.pointRef.phi) * Math.cos(this.pointRef.theta),
+        y: this.pointRef.radius * Math.cos(this.pointRef.phi),
+        z: this.pointRef.radius * Math.sin(this.pointRef.phi) * Math.sin(this.pointRef.theta),
+      }
+    );
     if (this.chamber == 1) {
       gsap.fromTo(
         this.camera.position,
-        { duration: 2, x: this.camera.position.x, y: this.camera.position.y, z: this.camera.position.z },
+        { duration: 5, x: this.camera.position.x, y: this.camera.position.y, z: this.camera.position.z },
         {
-          duration: 2,
+          duration: 5,
           x: this.ogPos[this.chamber - 1].x,
           y: this.ogPos[this.chamber - 1].y,
           z: this.ogPos[this.chamber - 1].z,
@@ -461,7 +494,7 @@ export class Sketch {
 
       gsap.fromTo(
         this.camera.position,
-        { x: this.camera.position.x, y: this.camera.position.y, z: this.camera.position.z },
+        { duration: 10, x: this.camera.position.x, y: this.camera.position.y, z: this.camera.position.z },
         {
           duration: 10,
           x: this.ogPos[this.chamber - 1].x,
@@ -485,9 +518,14 @@ export class Sketch {
 
   render() {
     if (this.controlPanel.VIEWmode == true && this.controlPanel.initialMove == false) {
-      this.camera.position.x = this.controlPanel.INTERSECTED.position.x + this.controlPanel.INTERSECTED.stare_dist * Math.cos(this.controlPanel.INTERSECTED.theta);
-      this.camera.position.y = this.controlPanel.INTERSECTED.position.y;
-      this.camera.position.z = this.controlPanel.INTERSECTED.position.z + this.controlPanel.INTERSECTED.stare_dist * Math.sin(this.controlPanel.INTERSECTED.theta);
+      gsap.to(this.camera.position, 5, {
+        x: this.controlPanel.INTERSECTED.position.x + this.controlPanel.INTERSECTED.stare_dist * Math.cos(this.controlPanel.INTERSECTED.theta),
+        y: this.controlPanel.INTERSECTED.position.y,
+        z: this.controlPanel.INTERSECTED.position.z + this.controlPanel.INTERSECTED.stare_dist * Math.sin(this.controlPanel.INTERSECTED.theta),
+      });
+      // this.camera.position.x = this.controlPanel.INTERSECTED.position.x + this.controlPanel.INTERSECTED.stare_dist * Math.cos(this.controlPanel.INTERSECTED.theta);
+      // this.camera.position.y = this.controlPanel.INTERSECTED.position.y;
+      // this.camera.position.z = this.controlPanel.INTERSECTED.position.z + this.controlPanel.INTERSECTED.stare_dist * Math.sin(this.controlPanel.INTERSECTED.theta);
     }
 
     if (this.controlPanel.VIEWmode == false && this.tiltCam == true) {
